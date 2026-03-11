8f3a02f9beee4ba569ec250ed4d2793152846f99
= <87355722+Cruzor-Blade@users.noreply.github.com>
2026-03-11 17:16:24 +0100

    Remove compiled JS artifacts from source directories
    Cleaned up .js files that were emitted by TypeScript compiler
    alongside .tsx/.ts sources. These are build artifacts that should
    not be in version control. The tsconfig already has noEmit: true
    to prevent this from recurring.

--- apps/docs/app/(home)/layout.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var home_1 = require("fumadocs-ui/layouts/home");
-var layout_shared_1 = require("@/lib/layout.shared");
-function Layout(_a) {
-    var children = _a.children;
-    return <home_1.HomeLayout {...(0, layout_shared_1.baseOptions)()}>{children}</home_1.HomeLayout>;
-}
-exports.default = Layout;
